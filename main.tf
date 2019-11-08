7c3bc3e4bab462afbd3ea6d045cb724b256cc341
Seun Awoga <seunsmooth@yahoo.com>
2019-11-08 16:42:59 +0000

    apps server update

--- main.tf
+++ main.tf
@@ -1,40 +1,61 @@
 data "template_file" "appserver_data" {
-data "template_file" "appserver-data" {
+
   template = "${file("template/appserver-data.tpl")}"
+
 }
 
-resource "aws_instance" "AppServerBox" {
+resource "aws_instance" "AppserverBox" {
+
   ami                         = "${var.ami_id}"
+
   instance_type               = "${var.instance_type}"
+
   associate_public_ip_address = true
+
   ebs_optimized               = false
+
   key_name                    = "${var.key_name}"
+
   user_data                   = "${data.template_file.appserver_data.rendered}"
-  user_data                   = "${data.template_file.appserver-data.rendered}"
+
   #subnet_id                   = "${aws_subnet.public[0]}"
-  subnet_id              = "${var.subnet_id}"
-  vpc_security_group_ids = ["${aws_security_group.apps_allow.id}"]
+
+  subnet_id              = "${data.terraform_remote_state.config.outputs.pri_subnet}"
+
+  vpc_security_group_ids = ["${aws_security_group.appserver_allow.id}"]
 
   tags = {
-    Name = "appserverBox"
-    Name = "appserverbox"
+
+    Name = "AppserverBox"
+
   }
+
 }
 
-resource "aws_security_group" "apps_allow" {
-  name        = "apps_allow_all"
-  name        = "app_allow_all"
+resource "aws_security_group" "appserver_allow" {
+
+  name        = "appserver_allow_all"
+
   description = "Allow all inbound traffic"
-  vpc_id      = "${var.vpc_id}"
+
+  vpc_id      = "${data.terraform_remote_state.config.outputs.vpc}"
+
+
 
   ingress {
+
     from_port   = 22
+
     to_port     = 22
+
     protocol    = "tcp"
+
     cidr_blocks = ["0.0.0.0/0"]
+
   }
 
   ingress {
+
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
@@ -54,8 +75,12 @@ resource "aws_security_group" "apps_allow" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags = {
-    Name = "Appserver SG"
-   }
+
+    Name = "AppserverBox SG"
+
+  }
+
 }
 
--- template/appserver-data.tpl
+++ template/appserver-data.tpl
@@ -1,4 +1,4 @@
-git #!/bin/bash
+#!/bin/bash
 set -e
 
 #do an update
@@ -21,4 +21,3 @@ sudo yum install nodejs -y
 
 curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
 sudo yum install yarn -y
-
