0c0ef39adbc81187d5c44b26243c28dc0de0b8db
Seun Awoga <seunsmooth@yahoo.com>
2019-11-08 23:35:01 +0000

    editing main file

--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
 output "appserver_node_endpoint" {
-  value = "http://${aws_instance.AppServerBox.public_ip}:8080  To connect ssh -i key ec2-user@${aws_instance.AppServerBox.public_ip}"
+  value = "http://${aws_instance.AppserverBox.public_ip}:8080  To connect ssh -i key ec2-user@${aws_instance.AppserverBox.public_ip}"
 }
